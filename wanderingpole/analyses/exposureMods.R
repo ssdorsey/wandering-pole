da8bf3660ab36d98335a707cb642fe123ed7f56f
Andy Ballard <andrew.ballard@duke.edu>
2021-01-17 11:12:32 -0600

    update exposure models

--- wanderingpole/analyses/exposureMods.R
+++ wanderingpole/analyses/exposureMods.R
@@ -29,9 +29,9 @@ tw <- readRDS('~/Dropbox/Projects/Twitter/modelData.rds')
 ### Models predicting exposure
 
 # Run models, save (takes about 40min per model)
-mRT <- speedlm(rtDiff ~ polarizing + memberPresPty + ideolDiffPos + absPVI + house + female + years + congress + icpsr, data=tw)
+mRT <- speedlm(rtDiff ~ uncivil + memberPresPty + ideolDiffPos + absPVI + house + female + years + congress + icpsr, data=tw)
 saveRDS(mRT, file='~/Dropbox/Projects/Twitter/rtMod.RData'); rm(mRT); gc()
-mFave <- speedlm(faveDiff ~ polarizing + memberPresPty + ideolDiffPos + absPVI + house + female + years + congress + icpsr, data=tw)
+mFave <- speedlm(faveDiff ~ uncivil + memberPresPty + ideolDiffPos + absPVI + house + female + years + congress + icpsr, data=tw)
 saveRDS(mFave, file='~/Dropbox/Projects/Twitter/faveMod.RData'); rm(mFave); gc()
 #mRTCiv <- speedlm(rtDiffCiv ~ polarizing + memberPresPty + ideolDiffPos + absPVI + house + female + years + congress + icpsr, data=tw)
 #saveRDS(mRTCiv, file='~/Dropbox/Projects/Twitter/rtCivMod.RData'); rm(mRTCiv); gc()
@@ -46,53 +46,53 @@ mFave <- readRDS('~/Dropbox/Projects/Twitter/faveMod.RData')
 ### Coefficient plots
 
 # Named vector to loop through (and a list to store the plots in)
-mods <- c(rtMod='Retweets above Average', rtCivMod='Retweets above Average (Civil)',
-          faveMod='Likes above Average', faveCivMod='Likes above Average (Civil)')
-coefPlots <- list()
-
-for(ii in 1:length(mods)){
-  # Load a model
-  mod <- readRDS(file=paste0('~/Dropbox/Projects/Twitter/', names(mods)[ii], '.RData'))
-
-  # Save coefficient plot as a ggplot object
-  modPlot <- plot_coefs(mod,
-                         coefs=c(`Incivility`='polarizing', `Ideological Extremity`='ideolDiffPos', `President's Party`='memberPresPty', `Electoral Safety`='absPVI', `House`='house', `Female`='female', `Seniority`='years'),
-                         scale=TRUE,
-                         inner_ci_level=0.9,
-                         colors=c('black')) +
-    xlab('Coefficient') +
-    ggtitle(mods[ii]) +
-    theme(plot.title = element_text(hjust = 0.5))
-
-  # Store ggplot object in the plots list
-  coefPlots[[ii]] <- modPlot
-
-  # Remove the model object, garbage clean
-  rm(mod); gc()
-}
-
-# Plot all together
-pdf('~/Dropbox/Projects/Twitter/engageModsAll.pdf', width=12, height=12)
-par(mfrow=c(2,2))
-do.call(grid.arrange, c(coefPlots, list(layout_matrix=rbind( c(1, 2), c(3, 4) ) ) ) )
-#grid.arrange(coefPlots[[1]], coefPlots[[2]], coefPlots[[3]], coefPlots[[4]],
-             # layout_matrix=rbind(c(1, 2), c(3, 4)))
-par(mfrow=c(1,1))
-dev.off()
-
-# Plot just the effects for the overall average changes
-pdf('~/Dropbox/Projects/Twitter/engageMods.pdf', width=10, height=6)
-par(mfrow=c(2,2))
-grid.arrange(coefPlots[[1]], coefPlots[[3]], layout_matrix=rbind(c(1, 2)))
-par(mfrow=c(1,1))
-dev.off()
-
-# Plot just the effects for the changes relative to civil tweets
-pdf('~/Dropbox/Projects/Twitter/engageModsCivil.pdf', width=10, height=6)
-par(mfrow=c(2,2))
-grid.arrange(coefPlots[[2]], coefPlots[[4]], layout_matrix=rbind(c(1, 2)))
-par(mfrow=c(1,1))
-dev.off()
+# mods <- c(rtMod='Retweets above Average', rtCivMod='Retweets above Average (Civil)',
+#           faveMod='Likes above Average', faveCivMod='Likes above Average (Civil)')
+# coefPlots <- list()
+# 
+# for(ii in 1:length(mods)){
+#   # Load a model
+#   mod <- readRDS(file=paste0('~/Dropbox/Projects/Twitter/', names(mods)[ii], '.RData'))
+# 
+#   # Save coefficient plot as a ggplot object
+#   modPlot <- plot_coefs(mod,
+#                          coefs=c(`Incivility`='polarizing', `Ideological Extremity`='ideolDiffPos', `President's Party`='memberPresPty', `Electoral Safety`='absPVI', `House`='house', `Female`='female', `Seniority`='years'),
+#                          scale=TRUE,
+#                          inner_ci_level=0.9,
+#                          colors=c('black')) +
+#     xlab('Coefficient') +
+#     ggtitle(mods[ii]) +
+#     theme(plot.title = element_text(hjust = 0.5))
+# 
+#   # Store ggplot object in the plots list
+#   coefPlots[[ii]] <- modPlot
+# 
+#   # Remove the model object, garbage clean
+#   rm(mod); gc()
+# }
+# 
+# # Plot all together
+# pdf('~/Dropbox/Projects/Twitter/engageModsAll.pdf', width=12, height=12)
+# par(mfrow=c(2,2))
+# do.call(grid.arrange, c(coefPlots, list(layout_matrix=rbind( c(1, 2), c(3, 4) ) ) ) )
+# #grid.arrange(coefPlots[[1]], coefPlots[[2]], coefPlots[[3]], coefPlots[[4]],
+#              # layout_matrix=rbind(c(1, 2), c(3, 4)))
+# par(mfrow=c(1,1))
+# dev.off()
+# 
+# # Plot just the effects for the overall average changes
+# pdf('~/Dropbox/Projects/Twitter/engageMods.pdf', width=10, height=6)
+# par(mfrow=c(2,2))
+# grid.arrange(coefPlots[[1]], coefPlots[[3]], layout_matrix=rbind(c(1, 2)))
+# par(mfrow=c(1,1))
+# dev.off()
+# 
+# # Plot just the effects for the changes relative to civil tweets
+# pdf('~/Dropbox/Projects/Twitter/engageModsCivil.pdf', width=10, height=6)
+# par(mfrow=c(2,2))
+# grid.arrange(coefPlots[[2]], coefPlots[[4]], layout_matrix=rbind(c(1, 2)))
+# par(mfrow=c(1,1))
+# dev.off()
 
 
 
@@ -103,9 +103,9 @@ dev.off()
 effPlotDat <- function(model){
   # Pull coefficient
   coefs <- summary(model)$coefficients
-  coef <- coefs['polarizing','coef']
+  coef <- coefs['uncivil','coef']
   # Pull CI
-  ci <- coefs['polarizing', 'se']*1.96
+  ci <- coefs['uncivil', 'se']*1.96
   # Combine/return
   effs <- c(coef=coef, ci=ci)
   effs
@@ -116,10 +116,10 @@ effs <- sapply(list(mFave, mRT), function(x){
   t()
 
 # Plot and save
-barCenters <- barplot(effs[,1], ylim=c(0, 250))
-pdf('~/Dropbox/Projects/Twitter/engageMods_effects.pdf', width=6, height=6)
+barCenters <- barplot(effs[,1], ylim=c(0, 350))
+pdf('~/Dropbox/Projects/Twitter/engageMods_effects_update.pdf', width=6, height=6)
 par(mar=c(3.1, 4.1, 2.1, 1.1))
-barplot(effs[,1], ylim=c(0, 250), ylab='Effect of Political Incivility', names.arg=c('Likes above\nAverage', 'Retweets above\nAverage'))
+barplot(effs[,1], ylim=c(0, 350), ylab='Effect of Political Incivility', names.arg=c('Likes above\nAverage', 'Retweets above\nAverage'))
 for(ii in 1:2){
   segments(x0=barCenters[ii], y0=effs[ii,1]-effs[ii,2], y1=effs[ii,1]+effs[ii,2], lwd=3)
   arrows(x0=barCenters[ii], y0=effs[ii,1]-effs[ii,2], y1=effs[ii,1]+effs[ii,2], lwd=3, angle=90, code=3, length=0.05)  
@@ -127,4 +127,3 @@ for(ii in 1:2){
 }
 dev.off()
 par(mar=c(5.1, 4.1, 4.1, 2.1))
-      
